01b77de7caa381014345bdb1f7ece78fd1ef7822
nikitabiichuk2009 <niktestpython@gmail.com>
2024-05-08 14:47:13 +0300

    Update Some Styling

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -6,18 +6,17 @@ function GetStarted() {
       className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient1 p-[2px] cursor-pointer`}
     >
       <div
-        className={`${styles.flexCenter} flex-col transition-colors duration-500 ease-in-out bg-primary w-[100%] hover:bg-[#121481]  h-[100%] rounded-full`}
+        className={`${styles.flexCenter} flex-col transition-colors duration-500 ease-in-out bg-primary w-[100%] hover:bg-[#0F1035]  h-[100%] rounded-full`}
       >
         <div className={`${styles.flexStart} flex-row `}>
           <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
             <span className="text-gradient ">Get</span>
           </p>
           <img src={arrowUp} className="w-[23px] h-[23px] object-contain" />
-          
         </div>
         <p className="font-poppins font-medium text-[18px] leading-[23px] ">
-            <span className="text-gradient">Started</span>
-          </p>
+          <span className="text-gradient">Started</span>
+        </p>
       </div>
     </div>
   );
